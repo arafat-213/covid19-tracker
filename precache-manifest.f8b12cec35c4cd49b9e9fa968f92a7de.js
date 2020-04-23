cb7eb5f8a23d9e783fb7d1843403d9304007953c
arafat-213 <tai.arafat.at@gmail.com>
2020-04-23 22:23:49 +0530

    Updates

--- precache-manifest.f8b12cec35c4cd49b9e9fa968f92a7de.js
+++ precache-manifest.f8b12cec35c4cd49b9e9fa968f92a7de.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a8b45b4a246f8d88980d586b0111e5ad",
+    "revision": "d653702de888c5e2ef45ed38e0bacbd9",
     "url": "/covid19-tracker/index.html"
   },
   {
-    "revision": "54d8a991ef0f7322b5a2",
+    "revision": "ae9a33d125ba47898738",
     "url": "/covid19-tracker/static/css/2.e15f4f49.chunk.css"
   },
   {
-    "revision": "02acf99606660d0440fd",
+    "revision": "8dfc1eba215d91b07bc5",
     "url": "/covid19-tracker/static/css/main.459185e2.chunk.css"
   },
   {
-    "revision": "54d8a991ef0f7322b5a2",
-    "url": "/covid19-tracker/static/js/2.2f7c2807.chunk.js"
+    "revision": "ae9a33d125ba47898738",
+    "url": "/covid19-tracker/static/js/2.08a1825c.chunk.js"
   },
   {
     "revision": "c836aaadfe43dfd9fcb8ddeddfd61e18",
-    "url": "/covid19-tracker/static/js/2.2f7c2807.chunk.js.LICENSE.txt"
+    "url": "/covid19-tracker/static/js/2.08a1825c.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "02acf99606660d0440fd",
-    "url": "/covid19-tracker/static/js/main.a3c40f60.chunk.js"
+    "revision": "8dfc1eba215d91b07bc5",
+    "url": "/covid19-tracker/static/js/main.77068216.chunk.js"
   },
   {
     "revision": "335abe5b48d4998eb849",
