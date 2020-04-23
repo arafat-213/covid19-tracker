f21f48db5c01febc09ba8cc57da2a035e593c2d8
arafat-213 <tai.arafat.at@gmail.com>
2020-04-23 10:22:28 +0530

    added daily new cases in district table

--- src/components/DistrictTable.js
+++ src/components/DistrictTable.js
@@ -2,33 +2,19 @@ import React from 'react'
 import BootstrapTable from 'react-bootstrap-table-next'
 import useDistrictCases from '../hooks/useDistrictCases'
 
-const columns = [
-	{
-		dataField: 'district',
-		text: 'District',
-		sort: true
-	},
-	{
-		dataField: 'confirmed',
-		text: 'Confirmed',
-		sort: true
-	},
-	{
-		dataField: 'active',
-		text: 'Active',
-		sort: true
-	},
-	{
-		dataField: 'recovered',
-		text: 'Recovered',
-		sort: true
-	},
-	{
-		dataField: 'deceased',
-		text: 'Deaths',
-		sort: true
-	}
-]
+const dailyCasesFormatter = (cell, row, rowIndex, formatData) => {
+	let field = formatData.dataField
+	const data = formatData.tableData
+	const isUpdate = formatData.isUpdate
+	if (data[rowIndex].delta[field] !== 0 && isUpdate)
+		return (
+			<span>
+				{' '}
+				{`[+${data[rowIndex].delta[field]}] ${data[rowIndex][field]}`}{' '}
+			</span>
+		)
+	return <span> {`${data[rowIndex][field]}`} </span>
+}
 
 const DistrictTable = props => {
 	const response = useDistrictCases()
@@ -38,8 +24,63 @@ const DistrictTable = props => {
 	var data = response.filter(obj => obj.state === props.state)
 	if (data[0]) {
 		tableData = data[0].districtData
-		console.log('From comp', data[0].districtData)
+		console.log('From comp', tableData)
 	}
+
+	const columns = [
+		{
+			dataField: 'district',
+			text: 'District',
+			sort: true,
+			formatter: dailyCasesFormatter,
+			formatExtraData: {
+				tableData,
+				dataField: 'district'
+			}
+		},
+		{
+			dataField: 'confirmed',
+			text: 'Confirmed',
+			sort: true,
+			formatter: dailyCasesFormatter,
+			formatExtraData: {
+				tableData,
+				dataField: 'confirmed',
+				isUpdate: true
+			}
+		},
+		{
+			dataField: 'active',
+			text: 'Active',
+			sort: true,
+			formatter: dailyCasesFormatter,
+			formatExtraData: {
+				tableData,
+				dataField: 'active'
+			}
+		},
+		{
+			dataField: 'recovered',
+			text: 'Recovered',
+			sort: true,
+			formatter: dailyCasesFormatter,
+			formatExtraData: {
+				tableData,
+				dataField: 'recovered'
+			}
+		},
+		{
+			dataField: 'deceased',
+			text: 'Deaths',
+			sort: true,
+			formatter: dailyCasesFormatter,
+			formatExtraData: {
+				tableData,
+				dataField: 'deceased',
+				isUpdate: true
+			}
+		}
+	]
 
 	return (
 		<div>
