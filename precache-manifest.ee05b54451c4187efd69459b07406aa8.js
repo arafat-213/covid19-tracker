7944a7f0ff00f064541baf33343ca49b154af577
arafat-213 <tai.arafat.at@gmail.com>
2020-04-23 16:57:23 +0530

    Updates

---
+++ precache-manifest.ee05b54451c4187efd69459b07406aa8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebda7b6f2d27449a978116b17129a217",
+    "url": "/covid19-tracker/index.html"
+  },
+  {
+    "revision": "54d8a991ef0f7322b5a2",
+    "url": "/covid19-tracker/static/css/2.e15f4f49.chunk.css"
+  },
+  {
+    "revision": "bc736776b69f9ad9855b",
+    "url": "/covid19-tracker/static/css/main.459185e2.chunk.css"
+  },
+  {
+    "revision": "54d8a991ef0f7322b5a2",
+    "url": "/covid19-tracker/static/js/2.2f7c2807.chunk.js"
+  },
+  {
+    "revision": "c836aaadfe43dfd9fcb8ddeddfd61e18",
+    "url": "/covid19-tracker/static/js/2.2f7c2807.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc736776b69f9ad9855b",
+    "url": "/covid19-tracker/static/js/main.5241fdca.chunk.js"
+  },
+  {
+    "revision": "335abe5b48d4998eb849",
+    "url": "/covid19-tracker/static/js/runtime-main.d9b0c56d.js"
+  }
+]);
