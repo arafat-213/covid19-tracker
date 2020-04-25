15dc59826110cd09934c8a8ce1674f3b79590a27
arafat-213 <tai.arafat.at@gmail.com>
2020-04-25 18:41:12 +0530

    Updates

---
+++ precache-manifest.60b57d4848844692c99e49237717dde9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b494192e987721362f8eb0a107cb5738",
+    "url": "/covid19-tracker/index.html"
+  },
+  {
+    "revision": "1d366a81103df3430560",
+    "url": "/covid19-tracker/static/css/2.414dc33a.chunk.css"
+  },
+  {
+    "revision": "48651d3270fc534792e7",
+    "url": "/covid19-tracker/static/css/main.b5568713.chunk.css"
+  },
+  {
+    "revision": "1d366a81103df3430560",
+    "url": "/covid19-tracker/static/js/2.c23c7ec3.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/covid19-tracker/static/js/2.c23c7ec3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48651d3270fc534792e7",
+    "url": "/covid19-tracker/static/js/main.1d1e52fd.chunk.js"
+  },
+  {
+    "revision": "335abe5b48d4998eb849",
+    "url": "/covid19-tracker/static/js/runtime-main.d9b0c56d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/covid19-tracker/precache-manifest.e4a8d6d6de86ae9c2f3dbf37ff19ccd5.js"
+  "/covid19-tracker/precache-manifest.60b57d4848844692c99e49237717dde9.js"
 );
 
 self.addEventListener('message', (event) => {
