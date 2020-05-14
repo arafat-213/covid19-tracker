24229d4fccabdbffcbf2e8db3e005ebe7d73aa47
arafat-213 <tai.arafat.at@gmail.com>
2020-05-14 23:43:07 +0530

    Updates

---
+++ precache-manifest.da3eb78b63b80e3f615a425f9878a1fd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02c293de49f807eddb477e5ef730e9c2",
+    "url": "/covid19-tracker/index.html"
+  },
+  {
+    "revision": "ef4f5f0d1ed1b93f13cd",
+    "url": "/covid19-tracker/static/css/2.efcc9e8f.chunk.css"
+  },
+  {
+    "revision": "19b0d44826515b7ef21b",
+    "url": "/covid19-tracker/static/css/main.0bb241d0.chunk.css"
+  },
+  {
+    "revision": "ef4f5f0d1ed1b93f13cd",
+    "url": "/covid19-tracker/static/js/2.d576a5d9.chunk.js"
+  },
+  {
+    "revision": "77c9fe09814b52da3a9104c128191c21",
+    "url": "/covid19-tracker/static/js/2.d576a5d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19b0d44826515b7ef21b",
+    "url": "/covid19-tracker/static/js/main.6e53dd27.chunk.js"
+  },
+  {
+    "revision": "335abe5b48d4998eb849",
+    "url": "/covid19-tracker/static/js/runtime-main.d9b0c56d.js"
+  }
+]);
