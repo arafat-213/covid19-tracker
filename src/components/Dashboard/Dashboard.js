6c1e275154b236b687ee1052dcf2c61fc6e5e2c7
arafat-213 <tai.arafat.at@gmail.com>
2020-06-04 12:41:05 +0530

    fixed NaN and undefined numbers on dashboard

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -8,115 +8,91 @@ const Dashboard = ({ india, global, time, region }) => {
 	let data = region === 'India' ? india : global
 	const { delta, total, loading } = data
 	console.log(data)
+	if (delta) {
+		// Handles NaN and undefined values
+		delta.recovered = delta.recovered || 0
+		delta.deceased = delta.deceased || 0
+	}
 
 	return (
 		<div>
-			<div className='row'>
-				<div className='col-lg-3 col-sm-6'>
-					<div
-						className='card card-confirmed m-1 m-l-0'
-						data-aos='fade-right'>
-						<DashboardCard
-							// className="four wide colum"
-							card='Confirmed'
-							count={
-								!loading
-									? parseInt(total.confirmed).toLocaleString(
-											'en-IN'
-									  )
-									: 'Loading..'
-							}
-							dailyCount={
-								!loading && delta.confirmed
-									? parseInt(delta.confirmed).toLocaleString(
-											'en-IN'
-									  )
-									: 'Loading..'
-							}
-						/>
+			{!loading ? (
+				<div className='row'>
+					<div className='col-lg-3 col-sm-6'>
+						<div
+							className='card card-confirmed m-1 m-l-0'
+							data-aos='fade-right'>
+							<DashboardCard
+								// className="four wide colum"
+								card='Confirmed'
+								count={parseInt(total.confirmed).toLocaleString(
+									'en-IN'
+								)}
+								dailyCount={parseInt(
+									delta.confirmed
+								).toLocaleString('en-IN')}
+							/>
+						</div>
+					</div>
+
+					<div className='col-lg-3 col-sm-6'>
+						<div
+							className='card card-active  m-1  m-l-0'
+							data-aos='fade-down'>
+							<DashboardCard
+								// className="four wide colum"
+								card='Active'
+								count={(
+									total.confirmed -
+									total.recovered -
+									total.deceased
+								).toLocaleString('en-IN')}
+								dailyCount={(
+									delta.confirmed -
+									delta.recovered -
+									delta.deceased
+								).toLocaleString('en-IN')}
+							/>
+						</div>
+					</div>
+
+					<div className='col-lg-3 col-sm-6'>
+						<div
+							className='card card-recovered m-1 m-l-0'
+							data-aos='fade-up'>
+							<DashboardCard
+								// className="four wide colum"
+								card='Recovered'
+								count={parseInt(total.recovered).toLocaleString(
+									'en-IN'
+								)}
+								dailyCount={parseInt(
+									delta.recovered
+								).toLocaleString('en-IN')}
+							/>
+						</div>
+					</div>
+
+					<div className='col-lg-3 col-sm-6'>
+						<div
+							className='card card-deceased m-1 m-l-0'
+							data-aos='fade-left'>
+							<DashboardCard
+								// className="four wide colum card-deceased"
+								card='Deceased'
+								count={parseInt(total.deceased).toLocaleString(
+									'en-IN'
+								)}
+								dailyCount={parseInt(
+									delta.deceased
+								).toLocaleString('en-IN')}
+							/>
+						</div>
 					</div>
 				</div>
-
-				<div className='col-lg-3 col-sm-6'>
-					<div
-						className='card card-active  m-1  m-l-0'
-						data-aos='fade-down'>
-						<DashboardCard
-							// className="four wide colum"
-							card='Active'
-							count={
-								!loading
-									? (
-											total.confirmed -
-											total.recovered -
-											total.deceased
-									  ).toLocaleString('en-IN')
-									: 'Loading..'
-							}
-							dailyCount={
-								!loading
-									? (
-											delta.confirmed -
-											delta.recovered -
-											delta.deceased
-									  ).toLocaleString('en-IN')
-									: 'Loading..'
-							}
-						/>
-					</div>
-				</div>
-
-				<div className='col-lg-3 col-sm-6'>
-					<div
-						className='card card-recovered m-1 m-l-0'
-						data-aos='fade-up'>
-						<DashboardCard
-							// className="four wide colum"
-							card='Recovered'
-							count={
-								!loading
-									? parseInt(total.recovered).toLocaleString(
-											'en-IN'
-									  )
-									: 'Loading..'
-							}
-							dailyCount={
-								!loading
-									? parseInt(delta.recovered).toLocaleString(
-											'en-IN'
-									  )
-									: 'Loading..'
-							}
-						/>
-					</div>
-				</div>
-
-				<div className='col-lg-3 col-sm-6'>
-					<div
-						className='card card-deceased m-1 m-l-0'
-						data-aos='fade-left'>
-						<DashboardCard
-							// className="four wide colum card-deceased"
-							card='Deceased'
-							count={
-								!loading
-									? parseInt(total.deceased).toLocaleString(
-											'en-IN'
-									  )
-									: 'Loading..'
-							}
-							dailyCount={
-								!loading
-									? parseInt(delta.deceased).toLocaleString(
-											'en-IN'
-									  )
-									: 'Loading..'
-							}
-						/>
-					</div>
-				</div>
-			</div>
-
+			) : (
+				'Loading....'
+			)}
 			<div style={{ marginTop: '4px' }} data-aos='fade-up-left'>
 				<UpdateTime time={time} />
 				<br />
