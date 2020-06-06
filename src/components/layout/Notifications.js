68cc901bde3dc5e7f19b054301af63c0dc1e57e0
arafat-213 <tai.arafat.at@gmail.com>
2020-06-06 17:05:20 +0530

    fix loading spinner and add style on notifications

--- src/components/layout/Notifications.js
+++ src/components/layout/Notifications.js
@@ -5,9 +5,9 @@ import moment from 'moment'
 const Notifications = ({ notifications }) => {
 	return notifications.map(notification => (
 		<div key={notification.timestamp} className='mb-1'>
-			<li className='list-group-item p-2, pb-0'>
+			<li className='list-group-item p-2, pb-0 mb-0 alert alert-info'>
 				<p className='mb-0 text-muted pb-0'>
-					about{' '}
+					&#8226; about{' '}
 					{moment
 						.duration(
 							moment().diff(moment.unix(notification.timestamp))
